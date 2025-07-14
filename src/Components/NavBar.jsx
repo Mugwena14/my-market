f1d4f478acf24369619c80ebf334fdb1e638a029
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-14 13:36:31 +0200

    Getting the NavBar Done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,12 +4,19 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            <h3>Yoo Loop</h3>
-            <Link to="/index">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/buy">Buy</Link>
-            <button>Sell</button>
+            <div className={styles.nav}>
+                <div className={styles.logo}>
+                <h3>Yoo Loop</h3>
+                </div>
+                <div className={styles.links}>
+                <Link className={styles.link} to="/index">Home</Link>
+                <Link className={styles.link} to="/about">About</Link>
+                <Link className={styles.link} to="/buy">Explore</Link>
+                <button>Sell</button>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
