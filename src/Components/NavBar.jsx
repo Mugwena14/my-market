bef38c672d7d434edd1ecd9b3f1997878f1c09e2
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-16 16:53:52 +0200

    Toggle works consecutively

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,13 @@
 import styles from '../Components/NavBar.module.css'
 import { Link } from 'react-router-dom'
 import { IoMenuOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import { useState } from 'react';
 
 const NavBar = () => {
 
     const [showFull, setShowFull] = useState(false);
+    const [hide, setHide] = useState(false);
 
     function showSideBar(){
         setShowFull(prevState => !prevState);
@@ -22,7 +24,16 @@ const NavBar = () => {
                     <Link className={styles.link} to="/about">About</Link>
                     <Link className={styles.link} to="/buy">Explore</Link>
                     <button>Sell</button>
-                    <a onClick={() => showSideBar()} className={styles.sideMenu}><IoMenuOutline /></a>
+                    {showFull ? (
+                        <>
+                            <a onClick={() => showSideBar()} className={styles.hideMenu}><IoMdClose /></a>
+                        </>
+                    ) : (
+                        <>
+                        <a onClick={() => showSideBar()} className={styles.sideMenu}><IoMenuOutline /></a>
+                        
+                        </>
+                    )}
                 </div>
             </div>
                         {showFull ? (
@@ -33,7 +44,7 @@ const NavBar = () => {
                                         <Link className={styles.sideLinks} to="/index">Home</Link>
                                         <Link className={styles.sideLinks} to="/about">About</Link>
                                         <Link className={styles.sideLinks} to="/buy">Explore</Link>
-                                        <button className={styles.sideButton}>Sell</button>
+                                        <Link className={styles.sideLinks} to="/buy">Buy</Link>
                                     </div>
                                 </div>
                             </div>
