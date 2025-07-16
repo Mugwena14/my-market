8503e04b93dffd75bfdbd5757d6208b04daa2297
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-16 16:27:52 +0200

    The toggle now works

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,16 @@
 import styles from '../Components/NavBar.module.css'
 import { Link } from 'react-router-dom'
 import { IoMenuOutline } from "react-icons/io5";
+import { useState } from 'react';
 
 const NavBar = () => {
+
+    const [showFull, setShowFull] = useState(false);
+
+    function showSideBar(){
+        setShowFull(prevState => !prevState);
+    }
+
     return (
         <div className={styles.fixed}>
             <div className={styles.nav}>
@@ -10,23 +18,27 @@ const NavBar = () => {
                 <h3>Yoo Loop</h3>
                 </div>
                 <div className={styles.links}>
-                <Link className={styles.link} to="/index">Home</Link>
-                <Link className={styles.link} to="/about">About</Link>
-                <Link className={styles.link} to="/buy">Explore</Link>
-                <button>Sell</button>
-                <a onClick={} className={styles.sideMenu}><IoMenuOutline /></a>
+                    <Link className={styles.link} to="/index">Home</Link>
+                    <Link className={styles.link} to="/about">About</Link>
+                    <Link className={styles.link} to="/buy">Explore</Link>
+                    <button>Sell</button>
+                    <a onClick={() => showSideBar()} className={styles.sideMenu}><IoMenuOutline /></a>
                 </div>
             </div>
-            <div className={styles.side}>
-                <div className={styles.col}>
-                    <div className={styles.sideLink}>
-                    <Link className={styles.sideLinks} to="/index">Home</Link>
-                    <Link className={styles.sideLinks} to="/about">About</Link>
-                    <Link className={styles.sideLinks} to="/buy">Explore</Link>
-                    <button className={styles.sideButton}>Sell</button>
-                    </div>
-                </div>
-            </div>
+                        {showFull ? (
+                            <>
+                            <div className={styles.side}>
+                                <div className={styles.col}>
+                                    <div className={styles.sideLink}>
+                                        <Link className={styles.sideLinks} to="/index">Home</Link>
+                                        <Link className={styles.sideLinks} to="/about">About</Link>
+                                        <Link className={styles.sideLinks} to="/buy">Explore</Link>
+                                        <button className={styles.sideButton}>Sell</button>
+                                    </div>
+                                </div>
+                            </div>
+                            </>
+                        ) : ''}
         </div>
         
     )
