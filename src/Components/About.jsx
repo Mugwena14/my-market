defb487d970776c74e366dad4eef9fe17f423d96
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-17 12:28:00 +0200

    Did the About CSS

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,15 +3,16 @@ import styles from '../Components/About.module.css'
 const About = () => {
     return (
         <div>
-            <div className={styles.about}>
-                <h2>About this Market-Place:</h2>
-            </div>
             <div className={styles.aboutCont}>
                 <div className={styles.contLeft}>
-                    <p>About Us</p>
-                    <h3>About this MarketPlace:</h3>
+                    <div className={styles.aboutUs}>
+                        <h4 className={styles.aboutP}>
+                            About Us
+                        </h4>
+                    </div>
+                    <h2>About this MarketPlace:</h2>
                     <p>
-                        Welcome to your go-to free marketplace demo—a 
+                        Welcome to your go-to free marketplace demo - a 
                         platform designed to simplify buying and selling
                         between everyday users.
                         Catch the creator behind this build:
@@ -22,15 +23,15 @@ const About = () => {
                         Whether you're listing a 
                         product you no longer need or browsing for that 
                         hidden gem to be passed on, i have 
-                        made possible by letting the two parties
+                        made it possible by letting the two parties
                         chat before one commit.
                     </p>
                     <p>
-                        This version is a demo of what's to come. 🚧 In 
+                        This version is a demo of what's to come. In 
                         the upcoming update, we're adding powerful features
                         like product deletion, but not just for anyone. 
                         Only the original uploader will have the authority to 
-                        remove their listed items, ensuring secure and fair control. 🔐
+                        remove their listed items, ensuring secure and fair control.
                     </p>
                     <p>
                         To manage authentication while it's still in development,
