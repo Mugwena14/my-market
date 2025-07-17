30f7bf4eab70f6759607a6fc1e3d278cfbc8afc6
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-17 14:22:26 +0200

    Did the toggle to show more/less feature in the about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import styles from '../Components/About.module.css'
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { MdExpandMore } from "react-icons/md";
+import { MdExpandLess } from "react-icons/md";
 
 const About = () => {
+
+    const [showMore, setShowFull] = useState(true);
+
+    function onHandle(){
+        setShowFull(prevState => !prevState)
+    }
+
     return (
         <div>
             <div className={styles.aboutCont}>
@@ -16,30 +29,72 @@ const About = () => {
                         platform designed to simplify buying and selling
                         between everyday users.
                         Catch the creator behind this build:
-                        <div></div>
+                        <div className={styles.icons}>
+                            <Link className={styles.icon1} to=""><FaSquareGithub /></Link>
+                            <Link className={styles.icon2} to=""><FaLinkedin /></Link>
+                        </div>
                     </p>
                 </div>
                 <div className={styles.contRight}>
-                    <p>
+                    {showMore ? (
+                        <>
+                        <p>
+                        Whether you're listing a 
+                        product you no longer need or browsing for that 
+                        hidden gem to be passed on, i have 
+                        made it possible by letting the two parties
+                        chat before one's commit.
+                        </p>
+                        <p>
+                            This version is a demo of what's to come. In 
+                            the upcoming update, we're adding powerful features
+                            like product deletion, but not just for anyone. 
+                            Only the original up...
+                        </p>
+                        </>
+                    ) : (
+                        <>
+                        <p>
                         Whether you're listing a 
                         product you no longer need or browsing for that 
                         hidden gem to be passed on, i have 
                         made it possible by letting the two parties
                         chat before one commit.
-                    </p>
-                    <p>
-                        This version is a demo of what's to come. In 
-                        the upcoming update, we're adding powerful features
-                        like product deletion, but not just for anyone. 
-                        Only the original uploader will have the authority to 
-                        remove their listed items, ensuring secure and fair control.
-                    </p>
-                    <p>
-                        To manage authentication while it's still in development,
-                        all listed products will automatically disappear after 30 days. 
-                        This keeps the platform tidy and protects users as we fine-tune 
-                        those account-based permissions. Happy trade!
-                    </p>
+                        </p>
+                        <p>
+                            This version is a demo of what's to come. In 
+                            the upcoming update, we're adding powerful features
+                            like product deletion, but not just for anyone. 
+                            Only the original uploader will have the authority to 
+                            remove their listed items, ensuring secure and fair control.
+                        </p>
+                        <p>
+                            To manage authentication while it's still in development,
+                            all listed products will automatically disappear after 30 days. 
+                            This keeps the platform tidy and protects users as we fine-tune 
+                            those account-based permissions. Happy trade!
+                        </p>
+                        </>
+                    )}
+                    
+                <button onClick={() => onHandle()} className={styles.btn}>{showMore ? 
+                    <> 
+                        <span className={styles.show}>
+                            More
+                        </span>
+                        <span className={styles.toggle}>
+                        <MdExpandMore />
+                        </span>
+                    </> : 
+                    <> 
+                        <span className={styles.show}>
+                            Less
+                        </span>
+                        <span className={styles.toggle}>
+                        <MdExpandLess />
+                        </span>
+                    </>
+                    }</button>
                 </div>
             </div>
         </div>
