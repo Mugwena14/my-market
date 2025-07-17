ffce3bc93771e23ffe444704d21b291af8fb5763
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-17 15:19:06 +0200

    Deleted all text from footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,21 +6,30 @@ const  Footer = () => {
 
     return (
         <footer>
-            <div className={styles.top}>
-                <p>&copy; {currentYear} Built by Makhubele Langavi Clyde | Junior-Front-End Developer | React</p>
-                <p>Open to Full-Time Opportunities or Freelance roles, Let's Connect:</p>
-            </div>
-            <div className={styles.connect}>
-                <div className={styles.connectLeft}>
-                    <p>Email: <a href="mailto:mlangaviclyde@gmail.com">mlangaviclyde@gmail.com</a></p>
-                    <p>Phone: <a href="tel:+27 68 502 1117">+27 502 1117</a></p>
-                    <p>GitHub Profile: <a href="">link</a></p>
-                    <p>LinkedIn Profile: <a href="">link</a></p>
+            <div className={styles.footer}>
+                <div></div>
+                <div className={styles.cont}>
+                    <div className={styles.top}>
+                        <h3>CONTACT</h3>
+                    </div>
+                    <div>
+                        
+                    </div>
                 </div>
-                <div className={styles.connectRight}>
-                    <p><a href="">Resume / CV</a></p>
-                    <p><a href="">Certified Front-End-Developer</a></p>
-                    <p><a href="">My Portfolio:</a></p>
+                <div className={styles.cont}>
+                    <div className={styles.top}>
+                        <h3>HELP</h3>
+                    </div>
+                </div>
+                <div className={styles.cont}>
+                    <div className={styles.top}>
+                        <h3>CAREER</h3>
+                    </div>
+                </div>
+                <div className={styles.cont}>
+                    <div className={styles.top}>
+                        <h3>RESOURCES</h3>
+                    </div>
                 </div>
             </div>
         </footer>
