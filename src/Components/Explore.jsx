f20645a52566b7e608f487029f5be766d870ec99
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-18 11:02:41 +0200

    Added the card component in the explore page

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -1,10 +1,42 @@
 import styles from './Explore.module.css'
+import snd from '../assets/images/iPhone-16-Plus.png'
+import { CiLocationOn } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { CiDollar } from "react-icons/ci";
+
 
 const Explore = () => {
     return (
         <div>
             <div className={styles.top}>
                 <h2>Browse Products</h2>
+            </div>
+            <div className={styles.featureCont}>
+                <div className={styles.card}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
