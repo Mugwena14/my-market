821ac15e480c7f8f6408117c04872801be907d97
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-18 12:14:25 +0200

    Added the style to the explore cards and made them responsive

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -7,12 +7,287 @@ import { CiDollar } from "react-icons/ci";
 
 const Explore = () => {
     return (
-        <div>
+        <div className={styles.main}>
             <div className={styles.top}>
                 <h2>Browse Products</h2>
             </div>
-            <div className={styles.featureCont}>
-                <div className={styles.card}>
+            <div className={styles.container}>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
+                    <div className={styles.image}>
+                        <img src={snd} alt="" />
+                    </div>
+                    <div className={styles.info}>
+                        <p><CiUser />
+                        <span className={styles.tags}>
+                            NAME
+                        </span>
+                        </p>
+                        <p><CiLocationOn />
+                        <span className={styles.tags}>
+                            Location
+                        </span>
+                        </p>
+                        <div className={styles.bottom}>
+                            <p><CiDollar /> 
+                            <span className={styles.tags}>
+                                Price
+                            </span>
+                            </p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.featureCont}>
                     <div className={styles.image}>
                         <img src={snd} alt="" />
                     </div>
