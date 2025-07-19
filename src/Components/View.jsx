0a550805b3e6eebae57fadc887a419c85886c1f0
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-19 12:45:57 +0200

    Added react icons, View Page

--- src/Components/View.jsx
+++ src/Components/View.jsx
@@ -1,33 +1,46 @@
 import styles from './View.module.css'
 import snd from '../assets/images/iPhone-XR.png'
 import { Link } from 'react-router-dom'
+import { IoArrowBackOutline } from "react-icons/io5";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import { FaChevronDown } from "react-icons/fa";
+import { IoCallSharp } from "react-icons/io5";
+import { FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { IoChatboxOutline } from "react-icons/io5";
 
 const View = () => {
     return (
         <div className={styles.cont}>
             <div className={styles.box1}>
-                <p>Go back</p>
+                <p>
+                    <span><IoArrowBackOutline /></span>
+                    Go back
+                </p>
                 <img src={snd} alt="" />
             </div>
             <div className={styles.box2}>
                 <p>Langavi</p>
                 <h1>Apple iPhone 16</h1>
                 <h2>R17 000</h2>
-                <p>I'm available during morning hours</p>
+                <p><span><AiOutlineExclamationCircle /></span>
+                    I'm available during morning hours</p>
                 <div className={styles.description}>
                     <div className={styles.desHead}>
                         <h3>Product Description</h3>
+                        <FaChevronDown />
                     </div>
                     <p></p>
                 </div>
                 <div className={styles.contact}>
                     <div className={styles.contHead}>
                         <h3>Hit me up:</h3>
+                        <FaChevronDown />
                     </div>
                     <div className={styles.contacts}>
                         <div className={styles.social}>
                             <div className={styles.icon}>
-
+                                <IoCallSharp />
                             </div>
                             <div className={styles.info}>
                                 <p>Calls</p>
@@ -36,7 +49,7 @@ const View = () => {
                         </div>
                         <div className={styles.social}>
                             <div className={styles.icon}>
-
+                                <FaWhatsapp />
                             </div>
                             <div className={styles.info}>
                                 <p>whatsaap</p>
@@ -45,7 +58,7 @@ const View = () => {
                         </div>
                         <div className={styles.social}>
                             <div className={styles.icon}>
-
+                                <MdEmail />
                             </div>
                             <div className={styles.info}>
                                 <p>Email</p>
@@ -54,7 +67,7 @@ const View = () => {
                         </div>
                         <div className={styles.social}>
                             <div className={styles.icon}>
-
+                                <IoChatboxOutline />
                             </div>
                             <div className={styles.info}>
                                 <Link to=''>Let's Chat</Link>
