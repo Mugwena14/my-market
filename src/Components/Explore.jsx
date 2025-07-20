f1d15002fae5585825c0600ba6e779401ab3ad33
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-20 21:55:09 +0200

    Did the Explore Page CSS

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -6,13 +6,15 @@ const Explore = () => {
     return (
         <div className={styles.explore}>
             <div>
-                <h2>Browse Products</h2>
+                <h1>Browse Products</h1>
             </div>
             <div className={styles.container}>
                 <div className={styles.box}>
                     <img src={image} alt="" />
-                    <h3>Play Station 6</h3>
-                    <p>By Langavi</p>
+                    <div className={styles.info}>
+                        <h3>Play Station 6</h3>
+                        <p>By Langavi</p>
+                    </div>
                 </div>
             </div>
         </div>
