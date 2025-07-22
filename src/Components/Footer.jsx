e4acbc6bbb321d2d9328559b5cb58c273ab3f409
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-22 21:31:27 +0200

    Did the gfoote styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,15 +16,18 @@ const  Footer = () => {
                         </span>
                         KART
                     </h2>
-                    <p>Langavi</p>
-                    <p>Suggestions or just a simple hi? HMU @</p>
+                    <p className={styles.suggestions}>Questions, Suggestions or just a simple hi? HMU @loopkart.mugwena.co.za OR:</p>
                     <div className={styles.icons}>
                         <Link className={styles.icon1} to=""><FaSquareGithub /></Link>
                         <Link className={styles.icon2} to=""><FaLinkedin /></Link>
                     </div>
                     <div className={styles.important}>
-                        <p>Terms & Conditions</p>
-                        <p>&middot; Privacy Policy</p>
+                        <Link className={styles.terms}>
+                            <p>Terms & Conditions</p>
+                        </Link>
+                        <Link className={styles.conditions}>
+                            <p>&middot; Privacy Policy</p>
+                        </Link>
                         <p>&middot; All Rights Reserved</p>
                     </div>
                 </div>
